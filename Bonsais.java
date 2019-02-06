b54c0e0189d5dcd31ee418963f674d5fefedde2f
Jose Manuel <josemanuelvazquezescudero@gmail.com>
2019-02-06 12:23:47 +0100

    Creada clase bonsai y su constructor

---
+++ Bonsais.java
@@ -0,0 +1,25 @@
+/**
+ * Write a description of class Bonsais here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Bonsais
+{
+    // instance variables - replace the example below with your own
+    private String clase;
+    private boolean hojaPerenne;
+    private int años;
+    private double tamaño;
+    
+    /**
+     * Constructor for objects of class Bonsais
+     */
+    public Bonsais(String claseBonsai, boolean tienePerenne, int añosBonsai, double tamañoBonsai)
+    {
+        clase = claseBonsai;
+        hojaPerenne = tienePerenne;
+        años = añosBonsai;
+        tamaño = tamañoBonsai;
+    }
+}
